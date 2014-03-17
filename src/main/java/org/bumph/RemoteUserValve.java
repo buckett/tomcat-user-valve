f60305c84de9b84ee67e4ea000b6ab374ba71afc
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2014-03-17 11:11:53 +0000

    Removed unused import.

--- src/main/java/org/bumph/RemoteUserValve.java
+++ src/main/java/org/bumph/RemoteUserValve.java
@@ -3,7 +3,6 @@ package org.bumph;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
